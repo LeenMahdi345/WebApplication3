4f62a50ab11459a195d24ecc2faeb8af616051f0
شيرين فتحي <906461579@tulkarm.edu.ps>
2024-05-24 04:13:20 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Data;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDBcontext context= new ApplicationDBcontext(); 
+        public IActionResult Index()
+        {
+            var employees =context.Employees.ToList(); 
+            return View("Index", employees);
+        }
+    }
+}
---
+++ Data/ApplicationDBcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDBcontext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+         
+            optionsBuilder.UseSqlServer("server=.;database=asp8;trusted_connection=true;trustServerCertificate=true");
+        }
+    }
+}
